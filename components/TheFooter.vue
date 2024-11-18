c42a9443a51aaf7ba6f3de66e83cdd5813aedb52
Spatial Symmetry <spatialsymmetry@gmail.com>
2024-11-19 01:43:34 +0330

    empty commit

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,16 +1,13 @@
-
 <template>
   <footer p="y15 x15" flex="~ col gap4">
-    <div flex="~ row justify-between items-center">
-      <div flex="~ row gap2" items-center>
-        <img src="/movies-sm.webp" width="25" height="25" alt="Logo">
-        <div text-xl>
-          Symmetry Team Movies
-        </div>
+    <div flex="~ row gap2" items-center my2>
+      <img src="/movies-sm.webp" width="25" height="25" alt="Logo">
+      <div text-xl>
+        Symmetry Team Movies
       </div>
-      <div>
-        <LanguageSwitcher />
-      </div>
+    </div>
+    <div flex gap4 items-center>
+      <LanguageSwitcher />
     </div>
   </footer>
 </template>
